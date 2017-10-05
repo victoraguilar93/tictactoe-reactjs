0f7548388d6ba01e56949a510220b87b8c789a39
Victor Aguilar <valbert1993@gmail.com>
2017-10-05 13:49:52 -0600

    tic tac toe 3x3 working

--- src/index.js
+++ src/index.js
@@ -16,17 +16,27 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
+      hasWinner: false
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    let winner = false;
+
+    if (this.state.hasWinner || squares[i]) {
+      return false;
+    } else if (!this.state.hasWinner) {
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      winner = calculateWinner(squares);
+    }
+    
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
+      hasWinner: winner,
     });
-    calculateWinner(squares);
+
   }
   renderSquare(i) {
     return (
@@ -37,7 +47,13 @@ class Board extends React.Component {
     );
   }
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = this.state.hasWinner
+    let status;
+    if (winner) {
+      status = "Winner player: " + winner;
+    } else {
+      status = "Next player:  " + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -86,6 +102,22 @@ ReactDOM.render(
 );
 
 
-function calculateWinner() {
-  console.log(arguments[0])
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return false;
 }
